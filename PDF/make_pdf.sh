63411f072b15c78f964dbd15d736c1a62a9e6037
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2018-12-07 06:32:23 -0200

    preparing to build individual PDFs per chapter

--- PDF/make_pdf.sh
+++ PDF/make_pdf.sh
@@ -1,12 +1,16 @@
 echo "Generating lowres pages:"
-for f in ../originais/*.tiff
+for ch in ../originais/ch*
+do
+for f in $ch/*.tiff
 do
     echo "Converting $f ..."
     convert $f -resize 800 $f.lowres.jpg
 done
 
 echo "And now generating the lowres PDF..."
-convert ../originais/*.lowres.jpg How_to_Design_and_Build_Your_Own_Custom_TV_Games.pdf
-rm ../originais/*.lowres.jpg -f
+chapter=`basename $ch`
+convert $ch/*.lowres.jpg How_to_Design_and_Build_Your_Own_Custom_TV_Games_$chapter.pdf
+rm $ch/*.lowres.jpg -f
+done
 
 echo "done."
